e5092db75f21599b04520f9ac8ae114f1c37ce3f
Dmitry Frank <mail@dmitryfrank.com>
2017-12-17 02:05:51 +0200

    Improve docs for mgos_http_server.h
    PUBLISHED_FROM=9f2d99178e1b6790dbffaea79e84989192e8a62f

--- include/mgos_http_server.h
+++ include/mgos_http_server.h
@@ -16,13 +16,39 @@
 extern "C" {  // Make sure we have C-declarations in C++ programs
 #endif
 
-bool mgos_http_server_init(void);
-
+/*
+ * Return global listening connection
+ */
 struct mg_connection *mgos_get_sys_http_server(void);
 
-/* Register HTTP endpoint handler `handler` on URI `uri_path` */
+/*
+ * Register HTTP endpoint handler `handler` on URI `uri_path`
+ *
+ * Example:
+ * ```c
+ * static void foo_handler(struct mg_connection *c, int ev, void *p,
+ *                         void *user_data) {
+ *   (void) p;
+ *   if (ev != MG_EV_HTTP_REQUEST) return;
+ *   LOG(LL_INFO, ("Foo requested"));
+ *   mg_send_response_line(c, 200,
+ *                         "Content-Type: text/html\r\n");
+ *   mg_printf(c, "%s\r\n", "Fooooo");
+ *   c->flags |= (MG_F_SEND_AND_CLOSE | MGOS_F_RELOAD_CONFIG);
+ *   (void) user_data;
+ * }
+ *
+ * // Somewhere else:
+ * mgos_register_http_endpoint("/foo/", foo_handler, NULL);
+ * ```
+ */
 void mgos_register_http_endpoint(const char *uri_path,
                                  mg_event_handler_t handler, void *user_data);
+
+/*
+ * Like `mgos_register_http_endpoint()`, but additionally takes `struct
+ * mg_http_endpoint_opts opts`
+ */
 void mgos_register_http_endpoint_opt(const char *uri_path,
                                      mg_event_handler_t handler,
                                      struct mg_http_endpoint_opts opts);
